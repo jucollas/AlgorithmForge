e9ff50d290765285f042d558a5b6206aa1fbcaed
Osvarp <osvaargas@gmail.com>
2024-11-06 12:49:55 -0500

    Mi template por si acaso

---
+++ myTemplate_icpc.cpp
@@ -0,0 +1,36 @@
+	#include <bits/stdc++.h>
+
+typedef long long lint;
+
+using namespace std;
+
+#define rall(vec) vec.rbegin(), vec.rend()
+#define all(vec) vec.begin(), vec.end()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+
+//defined in header <random> :- mersenne-twister
+//mt19937_64 random_64( 121651631 );
+
+const int template_limit = 1e6;
+int a[template_limit], b[template_limit];
+
+void solve() {
+	
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout << setprecision(12) << fixed;
+
+    int t = 2;
+    cin >> t; ++t;
+    while ( --t ) {
+		solve();
+    }
+	return 0;
+}
+
