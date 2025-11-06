5c286b9188f6fa3a9b4a1099d41dd420342748dc
Osvarp <osvaargas@gmail.com>
2025-11-05 22:20:01 -0500

    preLatam

--- linalg/mat_primitives.cpp
+++ linalg/mat_primitives.cpp
@@ -11,7 +11,8 @@ mat mat_mult(const mat &m1,const mat &m2){
 	assert(!m2.empty()); assert(!m1.empty()); assert(m2.size()==m1[0].size());
 	int n = m1.size(),m=m2.size(),h=m2[0].size();
 	mat res(n,vector<tmat>(h,0));
-	// rep(i,0,n) rep(j,0,h) rep(k,0,m) res[i][j] += m1[i][k]*m2[k][j];
-	rep(i,0,n) rep(j,0,h) rep(k,0,m) res[i][j] = (res[i][j]+ m1[i][k]*1ll*m2[k][j])%mod;
+	// Adamant says this order is goty
+	// https://codeforces.com/blog/entry/129292
+	rep(i,0,n) rep(k,0,m) rep(j,0,h) res[i][j] = (res[i][j]+ m1[i][k]*1ll*m2[k][j])%mod;
 	return res;
 }
